b94e63ad3c8e901cf2fdf396a53d2392e813fb54
Sand82 <sandoki@abv.bg>
2024-07-04 12:00:10 +0300

    added new web project

---
+++ Server/OnlineStore.Web/OnlineStore.Web/Controllers/ItemController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.Web.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> GetItems() 
+        {
+            return new List<string>() { "Test", "Test1", "Test2", "Test3" };
+        }
+    }
+}
