71e9290d1112adf49e8ddd7c77c09d50622dc3e5
Sand82 <sandoki@abv.bg>
2024-08-24 21:27:01 +0300

    added Carousel

--- Client/online-store-client/src/components/Carousel/Carusel.tsx
+++ Client/online-store-client/src/components/Carousel/Carusel.tsx
@@ -2,30 +2,57 @@ import style from './Carousel.module.css'
 
 const Carousel = () => {
 
-    return (
+    return ( 
       <div className={style.carouselMainContainer}>
         <div className={style.carouselContainer}>
           <div className={style.carousel} id="carousel1">
-            <img src="/assets/img/image-1.png" alt="Builder repairing the roof" />
-            <section className='circleContainer'>
-              <div className={style.circle}></div>
-              <div className={style.circle}></div>
-              <div className={style.circle}></div>
-              <div className={style.circle}></div>
+            <section className={style.carouselScreen}>            
+              <img src="/assets/img/image-1.png" alt="Builder repairing the roof" />
+              <section className={style.textContainer}>
+                <p>Битови стоки</p>
+                <p>Всичко за дома и повече!</p>
+              </section>
             </section>
-            <div className={style.leftArrow}>
-              <span className={`${style.chevron} ${style.left}`}>
+            <section className={style.carouselScreen}>            
+              <img src="/assets/img/image-2.png" alt="Builder repairing the roof" />
+              <section className={style.textContainer}>
+                <p>Битови стоки</p>
+                <p>Всичко за дома и повече!</p>
+              </section>
+            </section>
+            <section className={style.carouselScreen}>            
+              <img src="/assets/img/image-3.png" alt="Builder repairing the roof" />
+              <section className={style.textContainer}>
+                <p>Битови стоки</p>
+                <p>Всичко за дома и повече!</p>
+              </section>
+            </section>
+            <section className={style.carouselScreen}>            
+              <img src="/assets/img/image-4.png" alt="Builder repairing the roof" />
+              <section className={style.textContainer}>
+                <p>Битови стоки</p>
+                <p>Всичко за дома и повече!</p>
+              </section>
+            </section>
+            <section className={style.circleContainer}>
+              <div className={style.circle}></div>
+              <div className={style.circle}></div>
+              <div className={style.circle}></div>
+              <div className={style.circle}></div>            
+            </section>
+            <div className={style.leftArrow} id="leftArrow">
+              <span className={`${style.chevron}  ${style.left}`}>
 
               </span>
             </div>
-            <div className={style.rightArrow}>
-              <span className={`${style.chevron} ${style.right}`}>
+            <div className={style.rightArrow} id="rightArrow">
+              <span className={`${style.chevron}  ${style.right}`}>
 
               </span>
             </div>
           </div>
         </div>
-      </div>
+      </div>      
     )
 }
 
